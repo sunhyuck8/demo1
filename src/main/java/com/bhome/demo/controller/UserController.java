fdb180e91c10af102acd1dfe3f412a125a69def0
Sun-Hyuck-Yoo <awfullysh82@gmail.com>
2023-07-20 17:14:23 +0900

    registform 까지 완료

--- src/main/java/com/bhome/demo/controller/UserController.java
+++ src/main/java/com/bhome/demo/controller/UserController.java
@@ -2,11 +2,22 @@ package com.bhome.demo.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @Controller
 public class UserController {
+    @GetMapping("myInfo/loginForm")
+    public String loginForm(HttpServletRequest request){
+        return "user/userLoginForm";
+    }
 
+    @GetMapping("myInfo/registForm")
+    public String registForm(HttpServletRequest request){
+        return "user/userRegistForm";
+    }
 
 }
