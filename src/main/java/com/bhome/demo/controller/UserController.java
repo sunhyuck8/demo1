bacd21ac6f5cff1fd0e2050ded043f8c49ab711d
Sun-Hyuck-Yoo <awfullysh82@gmail.com>
2023-07-22 04:36:25 +0900

    for 전송 까지 완료

--- src/main/java/com/bhome/demo/controller/UserController.java
+++ src/main/java/com/bhome/demo/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.bhome.demo.controller;
 
+import com.bhome.demo.dto.UserDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,9 +19,22 @@ public class UserController {
         return "user/userLoginForm";
     }
 
-    @GetMapping("myInfo/registForm")
-    public String registForm(HttpServletRequest request){
+    @GetMapping("/myInfo/registForm")
+    public String registForm(HttpServletRequest request,Model model){
+        model.addAttribute("userDto", new UserDto());//th:object를 쓰기 위함
         return "user/userRegistForm";
+    }
+
+    @RequestMapping(value = "/user/regist1", method = RequestMethod.POST)
+    public String userRegist(HttpServletRequest request,  UserDto userDto) throws JsonProcessingException {
+
+//        log.info("id와 password {}, {}",userDto.getUser_id(),userDto.getUser_pass());
+//        log.info("userdto 내용{}",request.getParameter("user_id"));
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(userDto);
+        log.info("userdto 내용{}",json);
+        return "/test";
     }
 
 }
